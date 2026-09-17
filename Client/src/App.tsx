91d36b9264ffe9594aae635466ed7786a9aa72dc
Ezra <ezrakissous26@gmail.com>
2026-09-17 16:16:32 +0300

    create register page

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -2,6 +2,12 @@ import { Route, Routes } from 'react-router'
 import './App.css'
 import Footer from './Components/Footer'
 import Header from './Components/Header'
+import GetPost from './Pages/GetPost'
+import Login from './Pages/Login'
+import Register from './Pages/register'
+import PostPost from './Pages/PostPost'
+
+
 
 function App() {
 
@@ -9,10 +15,10 @@ function App() {
     <>
     <Header/>
     <Routes>
-      <Route path='login' element={}/>
-      <Route path='register' element={}/>
-      <Route path='getpost' element={}/>
-      <Route path='postposts' element={}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='register' element={<Register/>}/>
+      <Route path='getpost' element={<GetPost/>}/>
+      <Route path='postposts' element={<PostPost/>}/>
     </Routes>
     <Footer/>
     </>
--- Client/src/Pages/Register.tsx
+++ Client/src/Pages/Register.tsx
@@ -1,13 +1,34 @@
-import React from 'react'
+import  { useState } from 'react'
+import { fetchRegister } from '../ServiceApi/api'
+// import { useNavigate } from 'react-router'
 
-export default function register() {
+export default function Register() {
+
+  const [username, setUsername ] = useState('')
+  const [email, setEmail ] = useState('')
+  const [password, setPassword] = useState('')
+  const [message, SetaMessage ] = useState('')
+  // const navigate = useNavigate()
+
+
+
   return (
-    <form>
-      <input type="text" placeholder='Enter your name'/>
-      <input type="email" name="" id="" placeholder='Enter you e-mail' />
-      <input type="password" name="" id="" placeholder='Enter your password' />
-      <input type="image" src="" alt="" />
+    <form onSubmit={ async (e) => {
+      e.preventDefault()
+      try {
+        const data = await fetchRegister(username, email, password)
+        SetaMessage(data.message)
+      } catch (error) {
+        SetaMessage(error.message)
+      }
+    }} >
+      <input onChange={(e) => setUsername(e.target.value)} value={username} type="text" placeholder='Enter your name'/>
+      <input onChange={(e) => setEmail(e.target.value)} value={email} type="email" name="" id="" placeholder='Enter you e-mail' />
+      <input onChange={(e) => setPassword(e.target.value)} value={password} type="password" name="" id="" placeholder='Enter your password' />
+      <input type="image" src="" alt="" placeholder='Drag here your image'/>
       <button type="submit">Submit</button>
+      {/* <button onClick={navigate('/login')} >Go to LogIn</button> */}
+      <p>{message}</p>
     </form>
   )
 }
